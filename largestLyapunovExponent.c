b87ea7495e6621649cf8298e7f1a323a360b8b39
TheBopDev <josh@bop.dev>
2024-10-08 14:43:16 -0500

    Boilerplate LLE stuff.

---
+++ largestLyapunovExponent.c
@@ -0,0 +1,6 @@
+/// Test
+///
+/// This code is an implementation of the following paper:
+/// https://physionet.org/files/lyapunov/1.0.0/RosensteinM93.pdf
+///
+/// I'm just writing the code. I did not create the algorithm.
